734a4313e5e014d78a45e5d89f7f562fa536b70c
Paul Larin <103615207+paullarinweb@users.noreply.github.com>
2022-11-08 19:19:25 +0800

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 "use strict";
-// data-action
+// counts
 let decrementOne = document.querySelectorAll(".counter__btn.dec")[0];
 
 let decrementTwo = document.querySelectorAll(".counter__btn.dec")[1];
@@ -8,16 +8,33 @@ let IncrementOne = document.querySelectorAll(".counter__btn.inc")[0];
 
 let IncrementTwo = document.querySelectorAll(".counter__btn.inc")[1];
 
+// inputs
 let InputOne = document.querySelectorAll(".counter__show")[0];
 let InputTwo = document.querySelectorAll(".counter__show")[1];
 
 // prices
 
-let ItemOnePrice = document.querySelectorAll(".card__price__new")[0];
-let ItemTwoPrice = document.querySelectorAll(".card__price__new")[1];
+let ItemOnePrice = Number(
+  document.querySelectorAll(".card__price__new")[0].textContent
+);
+
+let ItemTwoPrice = Number(
+  document.querySelectorAll(".card__price__new")[1].textContent
+);
+
+let TotalPrice = Number(
+  document.querySelectorAll(".checkout__total__shipping.total__shipping")[0]
+    .textContent
+);
+
+let TotalSpanPrice = document.querySelectorAll(
+  ".checkout__total__shipping.total__shipping"
+)[0];
 
 const priceFirstItem = 54.99;
 const priceSecondItem = 74.99;
+
+TotalSpanPrice.textContent = priceFirstItem + priceSecondItem;
 
 // decrements
 
@@ -25,7 +42,11 @@ decrementOne.addEventListener("click", function () {
   if (InputOne.value > 1) {
     InputOne.value -= 1;
     // change price item
-    ItemOnePrice.textContent = +ItemOnePrice.textContent - priceFirstItem;
+    ItemOnePrice = ItemOnePrice - priceFirstItem;
+    TotalPrice = ItemOnePrice + ItemTwoPrice;
+    // console.log(ItemOnePrice.toFixed(2));
+    // console.log(TotalPrice);
+    TotalSpanPrice.textContent = TotalPrice.toFixed(2);
   }
 });
 
@@ -33,7 +54,11 @@ decrementTwo.addEventListener("click", function () {
   if (InputTwo.value > 1) {
     InputTwo.value -= 1;
     // change price item
-    ItemTwoPrice.textContent = +ItemTwoPrice.textContent - priceSecondItem;
+    ItemTwoPrice = ItemTwoPrice - priceSecondItem;
+    TotalPrice = ItemOnePrice + ItemTwoPrice;
+    // console.log(ItemTwoPrice.toFixed(2));
+    // console.log(TotalPrice);
+    TotalSpanPrice.textContent = TotalPrice.toFixed(2);
   }
 });
 
@@ -43,7 +68,12 @@ IncrementOne.addEventListener("click", function () {
   if (InputOne.value >= 0) {
     InputOne.value = ++InputOne.value;
     // change price item
-    ItemOnePrice.textContent = priceFirstItem + +ItemOnePrice.textContent;
+    ItemOnePrice = priceFirstItem + ItemOnePrice;
+    TotalPrice = ItemOnePrice + ItemTwoPrice;
+
+    // console.log(ItemOnePrice.toFixed(2));
+    // console.log(TotalPrice);
+    TotalSpanPrice.textContent = TotalPrice.toFixed(2);
   }
 });
 
@@ -51,12 +81,10 @@ IncrementTwo.addEventListener("click", function () {
   if (InputTwo.value >= 0) {
     InputTwo.value = ++InputTwo.value;
     // change price item
-    ItemTwoPrice.textContent = priceSecondItem + +ItemTwoPrice.textContent;
+    ItemTwoPrice = priceSecondItem + ItemTwoPrice;
+    TotalPrice = ItemOnePrice + ItemTwoPrice;
+    // console.log(ItemTwoPrice.toFixed(2));
+    // console.log(TotalPrice);
+    TotalSpanPrice.textContent = TotalPrice.toFixed(2);
   }
 });
-
-console.dir(InputOne);
-
-console.dir(decrementOne);
-
-console.log(ItemOnePrice);
